688d2bba73253adb04a33315fe40cec71371aa8f
Timokrut <potapov.tim14@gmail.com>
2025-12-01 16:55:37 +0300

    Add database & db handler

---
+++ src/main/java/tracker/util/DB.java
@@ -0,0 +1,19 @@
+package main.java.tracker.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager; 
+
+public class DB {
+    private static final String URL = "jdbc:postgresql://localhost:5432/bugtracker";
+    private static final String USER = "my_user";
+    private static final String PASSWORD = "my_pass";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to connect to db", e);
+        }
+    }
+}
